ee906a16d616e9614cbd607263e858a8c37ae037
Jonathan Krug <jonathankrug@gmx.de>
2020-04-20 19:00:23 +0200

    added dev tests

---
+++ UniApp DevTests/UniApp_DevTests.swift
@@ -0,0 +1,21 @@
+//
+//  UniApp_DevTests.swift
+//  UniApp DevTests
+//
+//  Created by Jonathan Krug on 20.04.20.
+//  Copyright © 2020 Jonathan Krug. All rights reserved.
+//
+
+import XCTest
+
+class UniApp_DevTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
