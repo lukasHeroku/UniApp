6310296b5fa6f565281eb709c9af64291041cd4c
Jonathan Krug <jonathankrug@gmx.de>
2020-04-20 18:16:56 +0200

    first commit

---
+++ UniApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UniApp
+//
+//  Created by Jonathan Krug on 20.04.20.
+//  Copyright © 2020 Jonathan Krug. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UniApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UniApp
+//
+//  Created by Jonathan Krug on 20.04.20.
+//  Copyright © 2020 Jonathan Krug. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
